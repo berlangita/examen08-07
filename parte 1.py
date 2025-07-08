9b9337ca46eab84e854ae860100ff78c0d6f001d
berlangita <berlangito@gmail.com>
2025-07-08 09:34:13 +0200

    parte 3
    def holamundo(): define la función que se ejecutará cuando se acceda a la ruta.
    
    Crear el mensaje JSON:
    mensaje = {'mensaje': 'Hola Mundo desde Flask!'} crea un diccionario Python que contiene el mensaje a devolver.
    
    Convertir a JSON:
    return jsonify(mensaje) convierte el diccionario Python a formato JSON y lo devuelve como respuesta HTTP.

--- parte 1.py
+++ parte 1.py
@@ -1,3 +1,8 @@
 from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+@app.route('/holamundo')
+def holamundo():
+    mensaje = {'mensaje': 'Hola Mundo desde Flask!'}
+    return jsonify(mensaje)
